bf90971ac04c5fa46ec723e1839cb436938eae95
Dinuka Pramod <dinukah98@gmail.com>
2025-11-26 17:25:20 +0530

    add TypeScript fundamentals and primitive types to documentation

---
+++ freecodecamp/index.ts
@@ -0,0 +1,12 @@
+//primitive types in TS
+let myName: string = "John Doe";
+let age: number = 27;
+let isStudent: boolean = true;
+//other primitive types are null and undefined
+
+//defining a new custom types
+// use type keyword, name of type should start with capital letter
+
+//1
+type Food= string;
+let favFood: Food = "Pizza";
